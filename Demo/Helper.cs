f267b232a492071dc6e3ae77bbc3d2dddcaf1335
MinaHany256 <minahanyfoad@gmail.com>
2024-08-04 17:49:14 +0300

    Part 01 Generics - SWAP Example dd

---
+++ Demo/Helper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Helper
+    {
+        #region Non - Generic
+        //public static void SWAP(ref int X, ref int Y)
+        //{
+        //    Console.WriteLine("====== SWAP =======");
+        //    int Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //}
+
+        //public static void SWAP(ref double X, ref double Y)
+        //{
+        //    Console.WriteLine("====== SWAP =======");
+        //    double Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //}
+
+        //public static void SWAP(ref Point X, ref Point Y)
+        //{
+        //    Console.WriteLine("====== SWAP =======");
+        //    Point Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //} 
+        #endregion
+
+
+        // Generic Type
+        public static void SWAP<T>(ref T X, ref T Y)
+        {
+            Console.WriteLine("====== SWAP =======");
+            T Temp = X;
+            X = Y;
+            Y = Temp;
+        }
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Generic SWAP - Example 01
+            // In Case The GenericType "T" is Declared On Method Level Not (Class, Struct, Interface)
+            // Compiler Can Detect The GenericType "T" based on the Method Input Parameter Types.
+
+            /// int A = 3; int B = 5;
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+            /// Helper.SWAP/*<int>*/(ref A, ref B );
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+
+            /// double k = 3.3, L = 5.5;
+            /// Console.WriteLine($"k = {k}");
+            /// Console.WriteLine($"L = {L}");
+            /// Helper.SWAP<double>(ref k, ref L);
+            /// Console.WriteLine($"K = {k}");
+            /// Console.WriteLine($"L = {L}");
+
+            /// Point P1 = new Point(1,2);
+            /// Point P2 = new Point(5,6);
+            /// Console.WriteLine($"P1 = {P1}");
+            /// Console.WriteLine($"P2 = {P2}");
+            /// Helper.SWAP<Point>(ref P1, ref P2);
+            /// Console.WriteLine($"P1 = {P1}");
+            /// Console.WriteLine($"P2 = {P2}"); 
+            #endregion
+
+
         }
     }
 }
