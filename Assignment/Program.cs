ba3d0378d7fc7ba7d79ef47b7579f80214db93cb
MinaHany256 <minahanyfoad@gmail.com>
2024-08-05 11:13:37 +0300

    Question 03

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,33 @@
-﻿namespace Assignment
+﻿using System.Collections;
+
+namespace Assignment
 {
+
     internal class Program
     {
+        #region 3. You are given an ArrayList containing a sequence of elements. try to reverse the order of elements in the ArrayList in-place(in the same arrayList) without using the built-in Reverse. Implement a function that takes the ArrayList as input and modifies it to have the reversed order of elements.
+
+        //public static ArrayList ReverseArrayList(ArrayList Arr)
+        //{
+        //    int start = 0;
+        //    int end = Arr.Count - 1;
+
+        //    while (start < end)
+        //    {
+        //        object temp = Arr[start];
+        //        Arr[start] = Arr[end];
+        //        Arr[end] = temp;
+        //        start++;
+        //        end--;
+        //    }
+
+        //    return Arr;
+
+
+        //} 
+        #endregion
+
+
         static void Main(string[] args)
         {
             #region 1. The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
@@ -26,6 +52,16 @@
 
             //Console.WriteLine($"{value} is in Range : {result}");
             //Console.WriteLine($"Length = {length}");
+
+            #endregion
+
+            #region 3. You are given an ArrayList containing a sequence of elements. try to reverse the order of elements in the ArrayList in-place(in the same arrayList) without using the built-in Reverse. Implement a function that takes the ArrayList as input and modifies it to have the reversed order of elements.
+
+            //ArrayList Arr = new ArrayList() { 1, 2, 3, 4, 5 };
+            //ArrayList Result = ReverseArrayList(Arr);
+
+            //foreach (int i in Result)
+            //    Console.WriteLine(i);
 
             #endregion
 
