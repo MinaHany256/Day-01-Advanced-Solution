26f9dca66d7505ac615d692dc557ad316df511b3
MinaHany256 <minahanyfoad@gmail.com>
2024-08-05 10:47:31 +0300

    Question 02

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            #region The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
+            #region 1. The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
 
             //int[] numbers = { 8,6,2,4 };
 
@@ -13,6 +13,19 @@
             //foreach (int i in numbers)
             //    Console.WriteLine(i);
 
+
+            #endregion
+
+            #region 2. create a generic Range<T> class that represents a range of values from a minimum value to a maximum value. The range should support basic operations such as checking if a value is within the range and determining the length of the range.
+
+            //Range<int> range = new Range<int>(10, 20);
+
+            //int value = 5;
+            //bool result = range.IsInRange(value);
+            //int length = range.Length();
+
+            //Console.WriteLine($"{value} is in Range : {result}");
+            //Console.WriteLine($"Length = {length}");
 
             #endregion
 
---
+++ Assignment/Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Range<T> where T : IComparable<T>
+    {
+
+        public dynamic Min { get; set; }
+        public dynamic Max { get; set; }
+
+
+        public Range(T min, T max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public bool IsInRange(T Value)
+        {
+            return Value.CompareTo(Min) >= 0 && Value.CompareTo(Max) <= 0;
+        }
+
+        public T Length()
+        {
+            return Max - Min;
+        }
+
+    }
+}
