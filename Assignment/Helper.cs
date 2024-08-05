1352e3a6d4d5bec22c2cef7d4c656830c28bad9e
MinaHany256 <minahanyfoad@gmail.com>
2024-08-05 10:13:08 +0300

    Question 01

---
+++ Assignment/Helper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Helper<T> where T : IComparable<T>
+    {
+        public static void Sort(T[] Arr)
+        {
+            bool SWAP;
+
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                SWAP = false;
+
+                for (int j = 0; j < Arr.Length - 1 - i; j++)
+                {
+                    if (Arr[j].CompareTo(Arr[j + 1]) > 0)
+                    {
+                        T temp = Arr[j];
+                        Arr[j] = Arr[j + 1];
+                        Arr[j + 1] = temp;
+                        SWAP = true;
+                    }
+                }
+
+                if (SWAP == false)
+                    break;
+            }
+
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
+
+            //int[] numbers = { 8,6,2,4 };
+
+            //Helper<int>.Sort(numbers);
+
+            //foreach (int i in numbers)
+            //    Console.WriteLine(i);
+
+
+            #endregion
+
+
+
         }
     }
 }
